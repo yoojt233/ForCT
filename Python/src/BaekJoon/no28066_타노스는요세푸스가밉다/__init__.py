1639ddf0f2baf844ba347bcd5d54e19abfb2e7db
WhyPi <101052129+yoojt233@users.noreply.github.com>
2023-05-23 16:04:15 +0900

    add: Python ~2023.05.23 (#21)
    * add: ~2023.05.10
    
    * add: ~2023.05.11
    
    * fix: .gitignore file
    
    * add: ~2023.05.12
    
    * add: ~2023.05.13
    
    * fix: modify gitignore about Java's /out directory
    
    * add: ~2023.05.14
    
    * add: ~2023.05.15
    
    * add: ~2023.05.16
    
    * add: Python ~2023.05.18
    
    * add: Python ~2023.05.18
    
    * add: Python ~2023.05.19
    
    * add: Python ~2023.05.22
    
    * add: Python ~2023.05.23

---
+++ Python/src/BaekJoon/no28066_타노스는요세푸스가밉다/__init__.py
@@ -0,0 +1,19 @@
+import sys
+from collections import deque
+
+
+def adv_input():
+    return sys.stdin.readline().rstrip()
+
+
+n, k = map(int, adv_input().split())
+dq = deque(i for i in range(1, n + 1))
+
+while n > 1:
+    dq.rotate(-1)
+    for i in range(k - 1):
+        if n == 1: break
+        dq.popleft()
+        n -= 1
+
+print(dq.popleft())
