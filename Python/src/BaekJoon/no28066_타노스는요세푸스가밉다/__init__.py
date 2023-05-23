80e15100e89c83c8af5dcd71752e282a8838332a
유지태 <yoojt233@naver.com>
2023-05-23 16:12:34 +0900

    refactor: BOJ_no28066's code to short

--- Python/src/BaekJoon/no28066_타노스는요세푸스가밉다/__init__.py
+++ Python/src/BaekJoon/no28066_타노스는요세푸스가밉다/__init__.py
@@ -9,10 +9,9 @@ def adv_input():
 n, k = map(int, adv_input().split())
 dq = deque(i for i in range(1, n + 1))
 
-while n > 1:
+while n > k:
     dq.rotate(-1)
     for i in range(k - 1):
-        if n == 1: break
         dq.popleft()
         n -= 1
 
